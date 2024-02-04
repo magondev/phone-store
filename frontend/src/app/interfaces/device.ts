cfa9d8fbab8e84bab229288f3715be989cf71616
Magomed Achtaew <122747757+magondev@users.noreply.github.com>
2024-02-04 23:14:29 +0100

    frontend: loading newest-devices from api on landing-page

---
+++ frontend/src/app/interfaces/device.ts
@@ -0,0 +1,10 @@
+export interface Device {
+  id: string;
+  brand: string;
+  model: string;
+  storage: number;
+  displayType: string;
+  price: number;
+  operatingSystem: string;
+  releaseDate: string;
+}
--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { HeroComponent } from '../../components/hero/hero.component';
+import { CommonModule } from '@angular/common';
+import { Device } from '../../interfaces/device';
+
+interface DeviceResponse {
+  SAMSUNG: Device[];
+  HUAWEI: Device[];
+  APPLE: Device[];
+}
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HttpClientModule, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  appleDevices: Device[] = [];
+  samsungDevices: Device[] = [];
+  huaweiDevices: Device[] = [];
 
+  fetchAppleDevices() {
+    const endpointUrl = 'http://localhost:8080/api/v1/phones/newest-phones-per-brand';
+
+    this.http.get<DeviceResponse>(endpointUrl).subscribe((response: DeviceResponse) => {
+      if (response.APPLE) {
+        this.appleDevices = response.APPLE.map((device: Device) => device);
+      }
+
+      if (response.SAMSUNG) {
+        this.samsungDevices = response.SAMSUNG.map((device: Device) => device);
+      }
+
+      if (response.HUAWEI) {
+        this.huaweiDevices = response.HUAWEI.map((device: Device) => device);
+      }
+    });
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchAppleDevices();
+  }
 }
